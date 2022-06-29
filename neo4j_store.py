b2f74b7ae808a91018214724c23c6ab70b0cec44
A-baoYang <rubyyang0615@gmail.com>
2022-06-29 03:21:41 +0000

    revise: update store script in neo4j_store.py depends on new output schema

--- neo4j_store.py
+++ neo4j_store.py
@@ -7,7 +7,7 @@ from KGBuilder import neo4j
 print("====== Load Data ======")
 filepath = os.path.join(
     args["root_dir"], args["model_dir"], "output", 
-    data_args["module_FiNER_output_filename"]
+    data_args["module_EventOpt_output_filename"]
 )
 with open(filepath, "r", encoding="utf-8") as f:
     node_rels = json.load(f)
@@ -30,6 +30,7 @@ for item in tqdm(node_rels):
     entity_extract = article_node.pop("entity_extract")
     article_node.update({"type": "NEWS"})
     article_node.update({"name": article_node["title"]})
+    article_node.pop("event_relations")
     connection.create_node(node=article_node)
 
     for ent_type in tqdm(entity_extract.keys()):
@@ -39,33 +40,30 @@ for item in tqdm(node_rels):
             ent_node = {"type": ent_type, "name": name}
             connection.create_node(node=ent_node)
 
-    for event_item in tqdm(event_triplets):
+    for id, event_item in tqdm(event_triplets.items()):
         event_text = event_item["event"]
-        # EVENT_HEAD, EVENT_TAIL node
-        event_head = {"type": "EVENT_HEAD", "name": event_text[0]}
-        event_tail = {"type": "EVENT_TAIL", "name": event_text[2]}
-        connection.create_node(node=event_head)
-        connection.create_node(node=event_tail)
-        # EVENT_HEAD -事件關係-> EVENT_TAIL
-        event_rel = {"type": event_text[1]}
-        connection.create_relation(
-            ent1=event_head, ent2=event_tail, rel=event_rel
-        )
-        # EVENT_HEAD/EVENT_TAIL -涉及-> (entities)
+
+        # EVENT_TRIPLET node
+        event_tri_node = {"type": "EVENT_TRIPLET", "name": ",".join(event_text)}
+        connection.create_node(node=event_tri_node)
+
+        # EVENT_TRIPLET -涉及-> (entities)
         event_ent_extract = event_item["entity_extract"]
         _rel = {"type": "涉及"}
-        for _eve in [event_head, event_tail]:
-            for _type, _ent_list in event_ent_extract.items():
-                ent_mentioned = [{"type": _type, "name": ent} for ent in _ent_list if ent in _eve["name"]]
-                if ent_mentioned:
-                    for _node in tqdm(ent_mentioned):
-                        connection.create_relation(
-                            ent1=_eve, ent2=_node, rel=_rel
-                        )
+        for _type, _ent_list in event_ent_extract.items():
+            ent_mentioned = [
+                {"type": _type, "name": ent} for ent in _ent_list 
+                if ent in event_tri_node["name"]
+            ]
+            if ent_mentioned:
+                for _node in tqdm(ent_mentioned):
+                    connection.create_relation(
+                        ent1=event_tri_node, ent2=_node, rel=_rel
+                    )
 
-            # NEWS -涉及-> EVENT_HEAD
-            connection.create_relation(
-                ent1=article_node, ent2=_eve, rel=_rel
-            )
+        # NEWS -涉及-> EVENT_HEAD
+        connection.create_relation(
+            ent1=article_node, ent2=event_tri_node, rel=_rel
+        )
 
 print("Nodes & relations stored to Neo4j successfully.")
